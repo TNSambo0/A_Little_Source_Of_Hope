471c8974b6c53c95369beb6c04c80e9999ead4f2
Mishumo Mbulawa <mishumo1801@gmail.com>
2023-07-28 15:08:46 +0200

    Edited the Orphanage dashboard and Create pages

--- A_Little_Source_Of_Hope/Controllers/OrphanageController.cs
+++ A_Little_Source_Of_Hope/Controllers/OrphanageController.cs
@@ -1,10 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using A_Little_Source_Of_Hope.Data;
 namespace A_Little_Source_Of_Hope.Models
 {
     public class OrphanageController : Controller
     {
+        private readonly AppDbContext _AppDb;
+        public OrphanageController(AppDbContext appDb)
+        {
+            _AppDb = appDb;
+
+        }
         public IActionResult Index()
+        {
+            IEnumerable<Orphanage> orphanages = _AppDb.Orphanage;
+            return View();
+        }public IActionResult Create()
         {
             return View();
         }
--- A_Little_Source_Of_Hope/Models/Orphanage.cs
+++ A_Little_Source_Of_Hope/Models/Orphanage.cs
@@ -15,6 +15,14 @@ namespace A_Little_Source_Of_Hope.Models
         [DataType(DataType.Text)]
         public string Manager { get; set; }
         [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Orphanage Email")]
+        public string OrphanageEmail { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Physical Address")]
+        public string OrphanageAddress { get; set; }
+        [Required]
         [DataType(DataType.PhoneNumber)]
         [Display(Name = "Cell No")]
         public string  CellNumber { get; set; }
