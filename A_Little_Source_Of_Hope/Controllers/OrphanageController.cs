1c52781055957780718fd9bb2451d8655d9aa8f3
TNSambo <216017134@student.uj.ac.za>
2023-07-29 01:23:25 +0200

    Fixed the bugs for add to cart, remove from cart, clear cart, and change quantity of item in cart.

--- A_Little_Source_Of_Hope/Controllers/OrphanageController.cs
+++ A_Little_Source_Of_Hope/Controllers/OrphanageController.cs
@@ -1,26 +1,121 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using A_Little_Source_Of_Hope.Data;
+using A_Little_Source_Of_Hope.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+
 namespace A_Little_Source_Of_Hope.Models
 {
     public class OrphanageController : Controller
     {
+        private readonly ILogger<OrphanageController> _logger;
         private readonly AppDbContext _AppDb;
-        public OrphanageController(AppDbContext appDb)
-        {
-            _AppDb = appDb;
+        private readonly UserManager<AppUser> _userManager;
+        protected IAuthorizationService _AuthorizationService;
+        private readonly SignInManager<AppUser> _signInManager;
 
+        public OrphanageController(ILogger<OrphanageController> logger, AppDbContext AppDb, UserManager<AppUser> userManager,
+            IAuthorizationService AuthorizationService, SignInManager<AppUser> signInManager)
+        {
+            _logger = logger;
+            _AppDb = AppDb;
+            _userManager = userManager;
+            _AuthorizationService = AuthorizationService;
+            _signInManager = signInManager;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var orphanages = _AppDb.Orphanage;
-            if (_AppDb.Orphanage.Any())
+            try
             {
-                ViewData["orphanages"] = "not null";
+                var sessionHandler = new SessionHandler();
+                await sessionHandler.GetSession(HttpContext, _signInManager, _logger);
+                var user = await _userManager.GetUserAsync(User);
+                if (user == null)
+                {
+                    await sessionHandler.SignUserOut(_signInManager, _logger);
+                    return Problem("Please try login in again.");
+                }
+                var isAuthorized = User.IsInRole(Constants.ProductAdministratorsRole);
+                if (!isAuthorized)
+                {
+                    TempData["error"] = "You don't have the permission to see Product.";
+                    return RedirectToAction("Index", "Admin");
+                }
+                IEnumerable<Orphanage> objOrphanage = _AppDb.Orphanage;
+                if (_AppDb.Orphanage.Any()) { ViewData["orphanages"] = "not null"; }
+                else { ViewData["orphanages"] = null; }
+                return View(objOrphanage);
             }
-            return View(orphanages);
-        }public IActionResult Create()
+            catch (Exception ex)
+            {
+                if (ex != null)
+                {
+                    ViewData["error"] = ex.ToString();
+                }
+                return View();
+            }
+        }
+        public async Task<IActionResult> Create()
         {
+            var sessionHandler = new SessionHandler();
+            await sessionHandler.GetSession(HttpContext, _signInManager, _logger);
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                await sessionHandler.SignUserOut(_signInManager, _logger);
+                return Problem("Please try login in again.");
+            }
+            Orphanage orphanage = new();
+            var isAuthorized = await _AuthorizationService.AuthorizeAsync(User, orphanage, ProductOperations.Create);
+            if (!isAuthorized.Succeeded)
+            {
+                TempData["error"] = "You don't have the permission to create a product.";
+                return RedirectToAction("Index");
+            }
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Orphanage orphanage)
+        {
+
+            try
+            {
+                var sessionHandler = new SessionHandler();
+                await sessionHandler.GetSession(HttpContext, _signInManager, _logger);
+                if (ModelState.IsValid)
+                {
+                    var user = await _userManager.GetUserAsync(User);
+                    if (user == null)
+                    {
+                        await sessionHandler.SignUserOut(_signInManager, _logger);
+                        return Problem("Please try login in again.");
+                    }
+                    var orphanageFromDb = _AppDb.Orphanage.Contains(orphanage);
+                    if (orphanageFromDb != true)
+                    {
+                        await _AppDb.Orphanage.AddAsync(orphanage);
+                        await _AppDb.SaveChangesAsync();
+                        TempData["success"] = "Orphanage successfully created";
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Orphanage already exists.");
+                        TempData["error"] = "Orphanage already exists";
+                        return View(orphanage);
+                    }
+                }
+                return View(orphanage);
+            }
+            catch (Exception ex)
+            {
+                if (ex != null)
+                {
+                    ViewData["error"] = ex.ToString();
+                }
+                return View(orphanage);
+            }
         }
 
 
--- A_Little_Source_Of_Hope/Controllers/VolunteerController.cs
+++ A_Little_Source_Of_Hope/Controllers/VolunteerController.cs
@@ -1,23 +1,105 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using A_Little_Source_Of_Hope.Data;
+using A_Little_Source_Of_Hope.Areas.Identity.Data;
 
 namespace A_Little_Source_Of_Hope.Controllers
 {
     public class VolunteerController : Controller
     {
+        private readonly ILogger<VolunteerController> _logger;
+        private readonly AppDbContext _AppDb;
+        private readonly UserManager<AppUser> _userManager;
+        protected IAuthorizationService _AuthorizationService;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public VolunteerController(ILogger<VolunteerController> logger, AppDbContext AppDb, UserManager<AppUser> userManager,
+            IAuthorizationService AuthorizationService, SignInManager<AppUser> signInManager)
+        {
+            _logger = logger;
+            _AppDb = AppDb;
+            _userManager = userManager;
+            _AuthorizationService = AuthorizationService;
+            _signInManager = signInManager;
+        }
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult details()
+        public async Task<IActionResult> details(int id)
         {
-            return View();
+            var sessionHandler = new SessionHandler();
+            await sessionHandler.GetSession(HttpContext, _signInManager, _logger);
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                await sessionHandler.SignUserOut(_signInManager, _logger);
+                return Problem("Please try login in again.");
+            }
+            var isAuthorized = User.IsInRole(Constants.ProductAdministratorsRole);
+            if (!isAuthorized)
+            {
+                TempData["error"] = "You don't have the permission to see Product.";
+                return RedirectToAction("Index", "Admin");
+            }
+            var applicationFromDb = await _AppDb.Volunteer.FindAsync(id);
+            if (applicationFromDb == null)
+            {
+                return Problem("Application not found.");
+            }
+            return View(applicationFromDb);
         }
-        public async Task<IActionResult> Approve()
+        public async Task<IActionResult> Approve(int id)
         {
+            var sessionHandler = new SessionHandler();
+            await sessionHandler.GetSession(HttpContext, _signInManager, _logger);
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                await sessionHandler.SignUserOut(_signInManager, _logger);
+                return Problem("Please try login in again.");
+            }
+            var isAuthorized = User.IsInRole(Constants.ProductAdministratorsRole);
+            if (!isAuthorized)
+            {
+                TempData["error"] = "You don't have the permission to see Product.";
+                return RedirectToAction("Index", "Admin");
+            }
+            var applicationFromDb = await _AppDb.Volunteer.FindAsync(id);
+            if (applicationFromDb ==null)
+            {
+                return Problem("Application not found.");
+            }
+            applicationFromDb.Status = "Approved";
+            _AppDb.Volunteer.Update(applicationFromDb);
+            await _AppDb.SaveChangesAsync();
             return RedirectToAction("Index");
         }
-        public async Task<IActionResult> Reject()
+        public async Task<IActionResult> Reject(int id)
         {
+            var sessionHandler = new SessionHandler();
+            await sessionHandler.GetSession(HttpContext, _signInManager, _logger);
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                await sessionHandler.SignUserOut(_signInManager, _logger);
+                return Problem("Please try login in again.");
+            }
+            var isAuthorized = User.IsInRole(Constants.ProductAdministratorsRole);
+            if (!isAuthorized)
+            {
+                TempData["error"] = "You don't have the permission to see Product.";
+                return RedirectToAction("Index", "Admin");
+            }
+            var applicationFromDb = await _AppDb.Volunteer.FindAsync(id);
+            if (applicationFromDb == null)
+            {
+                return Problem("Application not found.");
+            }
+            applicationFromDb.Status = "Rejected";
+            _AppDb.Volunteer.Update(applicationFromDb);
+            await _AppDb.SaveChangesAsync();
             return RedirectToAction("Index");
         }
     }
